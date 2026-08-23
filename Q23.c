caa5bb9bacd7b956bd8fbd00fdea1effd14f609e
chaviigarg <chaviigarg25@gmail.com>
2026-08-24 02:54:28 +0530

    day 12-Q23

---
+++ Q23.c
@@ -0,0 +1,35 @@
+/*
+Q23. Write a program to calculate library fine based on late days:
+First 5 days late: Rs.2/day
+Next 5 days late: Rs.4/day
+Next 20 days late: Rs.6/day
+More than 30 days: Membership Cancelled.
+*/
+
+#include <stdio.h>
+
+int main() {
+    int days, fine;
+
+    scanf("%d", &days);
+    if (days <= 0) {
+        printf("No Fine");
+    }
+    else if (days <= 5) {
+        fine = days * 2;
+        printf("Fine = Rs.%d", fine);
+    }
+    else if (days <= 10) {
+        fine = (5 * 2) + (days - 5) * 4;
+        printf("Fine = Rs.%d", fine);
+    }
+    else if (days <= 30) {
+        fine = (5 * 2) + (5 * 4) + (days - 10) * 6;
+        printf("Fine = Rs.%d", fine);
+    }
+    else {
+        printf("Membership Cancelled");
+    }
+
+    return 0;
+}
