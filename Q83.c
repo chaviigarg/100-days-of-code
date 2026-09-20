f8478749a0cfb8c0a709853298b4c230fae23bd8
chaviigarg <chaviigarg25@gmail.com>
2026-09-21 00:58:02 +0530

    day 41 - Q83

---
+++ Q83.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+    char str[100];
+    int i = 0, vowels = 0, consonants = 0;
+
+    fgets(str, 100, stdin);
+
+    while(str[i] != '\0')
+    {
+        char ch = str[i];
+
+        if(ch >= 'A' && ch <= 'Z')
+            ch = ch + 32;
+
+        if(ch >= 'a' && ch <= 'z')
+        {
+            if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u')
+                vowels++;
+            else
+                consonants++;
+        }
+
+        i++;
+    }
+
+    printf("Vowels = %d\n", vowels);
+    printf("Consonants = %d", consonants);
+
+    return 0;
+}
